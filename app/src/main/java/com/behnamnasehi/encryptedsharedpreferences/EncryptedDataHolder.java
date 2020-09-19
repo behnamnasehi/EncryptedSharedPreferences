2543464dd1cc4fba85aeef6cc0942a57930ecd61
behnamnasehi <43644992+behnamnasehi@users.noreply.github.com>
2020-09-19 12:52:54 +0430

    init commit

--- app/src/main/java/com/behnamnasehi/encryptedsharedpreferences/EncryptedDataHolder.java
+++ app/src/main/java/com/behnamnasehi/encryptedsharedpreferences/EncryptedDataHolder.java
@@ -67,5 +67,4 @@ public class EncryptedDataHolder {
         editor.commit();
     }
 
-
 }
--- app/src/main/java/com/behnamnasehi/encryptedsharedpreferences/MainActivity.java
+++ app/src/main/java/com/behnamnasehi/encryptedsharedpreferences/MainActivity.java
@@ -1,14 +1,24 @@
 package com.behnamnasehi.encryptedsharedpreferences;
 
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    EncryptedDataHolder encryptedDataHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            encryptedDataHolder = new EncryptedDataHolder(this);
+        }
+        encryptedDataHolder.setApiKey("this is test from Behnam Nasehi");
+
+        TextView textView = findViewById(R.id.txt);
+        textView.setText(encryptedDataHolder.getApiKey());
     }
 }
